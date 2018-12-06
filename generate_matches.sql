97357f9c76dd52e86b20ebca3abc2645c922575d
jagadeeshdumpeta <44097695+jagadeeshdumpeta@users.noreply.github.com>
2018-12-06 15:21:45 +0530

    Update generate_matches.sql
    updated it by testing with the test data.

--- generate_matches.sql
+++ generate_matches.sql
@@ -1,8 +1,7 @@
 CREATE OR REPLACE PROCEDURE generate_matches()
 LANGUAGE plpgsql
 AS $$
-DECLARE 
-	
+
 DECLARE 
 	MatchGenerate CURSOR for Select userpk, age, lat,lon,PokemonCathRate,CatsOwned,LikeCats,Religion from users;
 	row  RECORD;
@@ -18,27 +17,32 @@ DECLARE
 	--2. when not all, inserts user matches based on the order of importance specified in the problem.
 	--orally This stores 2 matching users for each user every time it is called.
 		insert into UsersMatchSet 
-		select row.UserPK, userpk from users usm where age between (row.age-5, row.age+5) 
-		and ((row.CatsOwned>0 and LikeCats='true') or row.CatsOwned=0)
-		and ((PokemonCathRate*100>=row.PokemonCathRate*100-30) or (row.PokemonCathRate*100=70 and (row.PokemonCathRate-PokemonCathRate)*100<15))
-		and Religion = row.Religion
-		and (point(row.lat,row.lon) <@> point(usm.lon,usm.lat)) = (select DistanceInMiles from Configurations)
+		select row.UserPK, userpk from users usm where (point(row.lat,row.lon) <@> point(usm.lon,usm.lat)) = (select DistanceInMiles from Configurations)
+		and	usm.age >= row.age-5 and usm.age <= row.age+5
+		and ((row.CatsOwned>0 and usm.LikeCats='true') or row.CatsOwned=0)
+		and ((usm.PokemonCathRate*100>=row.PokemonCathRate*100-30) or (row.PokemonCathRate*100=70 and (row.PokemonCathRate-usm.PokemonCathRate)*100<15))
+		and usm.Religion = row.Religion		 
 		and not exists (select 1 from UsersMatchSet where UserID = row.UserPK and MatchedUserID = usm.userpk and IsMatched=1)
 		UNION
 		select row.UserPK, userpk from users usm 
-		where (point(row.lat,row.lon) <@> point(usm.lon,usm.lat)) = (select DistanceInMiles from Configurations)													   
+		where (point(row.lat,row.lon) <@> point(usm.lon,usm.lat)) = (select DistanceInMiles from Configurations)	
+		and not exists (select 1 from UsersMatchSet where UserID = row.UserPK and MatchedUserID = usm.userpk and IsMatched=1)
 		UNION
 		select row.UserPK, userpk from users usm 
-		where age >= row.age-5 and age <= row.age+5
+		where usm.age >= row.age-5 and usm.age <= row.age+5
+		and not exists (select 1 from UsersMatchSet where UserID = row.UserPK and MatchedUserID = usm.userpk and IsMatched=1)
 		UNION
 		select row.UserPK, userpk from users usm 
-		where ((row.CatsOwned>0 and LikeCats='true') or row.CatsOwned=0)
+		where ((row.CatsOwned>0 and usm.LikeCats='true') or row.CatsOwned=0)
+		and not exists (select 1 from UsersMatchSet where UserID = row.UserPK and MatchedUserID = usm.userpk and IsMatched=1)
 		UNION
 		select row.UserPK, userpk from users usm 
-		where ((PokemonCathRate*100>=row.PokemonCathRate*100-30) or (row.PokemonCathRate*100=70 and (row.PokemonCathRate-PokemonCathRate)*100<15))
+		where ((usm.PokemonCathRate*100>=row.PokemonCathRate*100-30) or (row.PokemonCathRate*100=70 and (row.PokemonCathRate-usm.PokemonCathRate)*100<15))
+		and not exists (select 1 from UsersMatchSet where UserID = row.UserPK and MatchedUserID = usm.userpk and IsMatched=1)
 		UNION
 		select row.UserPK, userpk from users usm 
-		where Religion = row.Religion	
+		where usm.Religion = row.Religion	
+		and not exists (select 1 from UsersMatchSet where UserID = row.UserPK and MatchedUserID = usm.userpk and IsMatched=1)
 		LIMIT 2;
 										
   END LOOP;
